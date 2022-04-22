6fe59c95d2c5d1b87e217ad7746d54baaec7f931
HongJieSunnn <457406475@qq.com>
2022-04-22 21:30:36 +0800

    Update CreateReviewedTagCommand and CreateReviewedTagCommandHandler in TagS.Microservices.Server.

--- TagS.Microservices.Server/Commands/CreateReviewedTagCommand.cs
+++ TagS.Microservices.Server/Commands/CreateReviewedTagCommand.cs
@@ -6,16 +6,18 @@
         public string TagDetail { get; set; }
         public string? PreviousTagId { get; set; }
         public List<string>? Ancestors { get; set; }
+        public List<string>? Synonyms { get; set; }
         public string UserId { get; set; }
-        public DateTime CreateTime { get; set; }
-        public CreateReviewedTagCommand(string preferredTagName,string tagDetail,string userId,DateTime createTime, string? previousTagId=null, List<string>? ancestors=null)
+        public DateTime? CreateTime { get; set; }
+        public CreateReviewedTagCommand(string preferredTagName,string tagDetail,string userId,DateTime? createTime, string? previousTagId=null, List<string>? ancestors=null,List<string>? synonyms=null)
         {
             PreferredTagName = preferredTagName;
             TagDetail = tagDetail;
             PreviousTagId = previousTagId;
             Ancestors = ancestors;
+            Synonyms = synonyms;
             UserId = userId;
-            CreateTime = createTime==default(DateTime)?DateTime.Now:createTime;
+            CreateTime = createTime ?? DateTime.Now;
         }
     }
 }
