ba7c9b896fbbc15648955ed2ba0fb5ce76d8c49b
HongJieSunnn <457406475@qq.com>
2022-04-22 21:34:45 +0800

    Add Integration events in TagS.Microservices.Server so that we need not use the integration events in TagS.Microservice.Client.

---
+++ TagS.Microservices.Server/IntegrationEventHandler/IntegrationEvents/AddReferrerToTagServerIntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿using TagS.Microservices.Client.Models;
+
+namespace TagS.Microservices.Server.IntegrationEventHandler.IntegrationEvents
+{
+    public record AddReferrerToTagServerIntegrationEvent:IntegrationEvent
+    {
+        public IReferrer Referrer { get; set; }
+        public string TagId { get; set; }
+        public AddReferrerToTagServerIntegrationEvent(IReferrer referrer, string tagId)
+        {
+            Referrer = referrer;
+            TagId = tagId;
+        }
+    }
+}
---
+++ TagS.Microservices.Server/IntegrationEventHandler/IntegrationEvents/RemoveReferrerToTagServerIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace TagS.Microservices.Server.IntegrationEventHandler.IntegrationEvents
+{
+    public record RemoveReferrerToTagServerIntegrationEvent : IntegrationEvent
+    {
+        public IReferrer Referrer { get; set; }
+        public string TagId { get; set; }
+        public RemoveReferrerToTagServerIntegrationEvent(IReferrer referrer, string tagId)
+        {
+            Referrer = referrer;
+            TagId = tagId;
+        }
+    }
+}
