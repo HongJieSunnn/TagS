f336829edb889feafb1c8b440e1e907b21fd2913
HongJieSunnn <457406475@qq.com>
2022-04-28 20:20:08 +0800

    Remove TagIntegrationEventService in TagS.Microservices.Client

--- TagS.Microservices.Client/Services/ITagIntegrationEventService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TagS.Microservices.Client.Services
-{
-    public interface ITagIntegrationEventService : IIntegrationEventService
-    {
-        Task PublishEventAsync(Guid integrationEventId);
-        Task PublishEventAsync(IEnumerable<Guid> integrationEventIds);
-    }
-}
