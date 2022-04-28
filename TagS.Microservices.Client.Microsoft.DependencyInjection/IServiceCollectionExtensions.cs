a375f93c394f494ced1eba6e919d15cac52cd63d
HongJieSunnn <457406475@qq.com>
2022-04-28 20:20:44 +0800

    Update IServiceCollectionExtensions in TagS.Microservices.Client.Microsoft.DependencyInjection

--- TagS.Microservices.Client.Microsoft.DependencyInjection/IServiceCollectionExtensions.cs
+++ TagS.Microservices.Client.Microsoft.DependencyInjection/IServiceCollectionExtensions.cs
@@ -1,15 +1,14 @@
 ﻿using EventBusCommon.Abstractions;
+using IntegrationEventServiceMongoDB.Extensions;
 using Microsoft.Extensions.DependencyInjection;
-using TagS.Microservices.Client.Services;
 
 namespace TagS.Microservices.Client.Microsoft.DependencyInjection
 {
     public static class IServiceCollectionExtensions
     {
-        public static IServiceCollection AddTagSClientMongoDBIntegrationEventService(this IServiceCollection services)
+        public static IServiceCollection AddTagSClient(this IServiceCollection services)
         {
-            services.AddScoped<ITagIntegrationEventService, TagIntegrationEventService>();
-            services.AddScoped<IIntegrationEventService, TagIntegrationEventService>();
+            
 
             return services;
         }
